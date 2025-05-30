08a4667f736c072a1000552b0347e260c044a58c
DaraKHib <darahibben@gmail.com>
2025-05-29 22:44:57 -0400

    updates

--- coach_calls/list_resources.py
+++ coach_calls/list_resources.py
@@ -16,8 +16,9 @@ def print_instance_ids(ec2_client):
     for instance_id in instance_ids:
         print(instance_id)
 
-ec2_client = get_ec2_client()
-s3_client = get_s3_client()
+if __name__=="__main__":
+    ec2_client = get_ec2_client()
+    s3_client = get_s3_client()
 
-#print_bucket_names(s3_client)
-print_instance_ids(ec2_client)
+    print_bucket_names(s3_client)
+    print_instance_ids(ec2_client)
